a5b22629c60204c3fecf09f2f75f6477a9886270
David Rusyniak <dmrusyniak@gmail.com>
2020-07-11 21:02:46 -0400

    header and nav initialized

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import logo from "../images/car-like-logo-2.png"
 
-import Header from "./header"
 import "./layout.css"
+
+const d = new Date()
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -17,23 +19,30 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <div className="container">
+      <div className="header">
+        <div className="header-left">
+          <div>
+            {d.getMonth()} &nbsp;-<span> </span>&nbsp;
+          </div>
+          <div>
+            {d.getDate()} &nbsp;-<span> </span>&nbsp;
+          </div>
+          <div>{d.getFullYear()} </div>
+        </div>
+        <img className="main-logo" src={logo} alt="Logo" />
+        <div className="header-right">
+          <a className="main-log-in">LOG IN</a>
+          <a className="main-sign-up">SIGN UP</a>
+        </div>
       </div>
-    </>
+      <div className="nav">
+        <div className="nav-element-first">HOME</div>
+        <div className="nav-element">CARS</div>
+        <div className="nav-element">ABOUT</div>
+      </div>
+      <main>{children}</main>
+    </div>
   )
 }
 
